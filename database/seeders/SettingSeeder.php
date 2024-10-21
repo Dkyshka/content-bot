16daaeda98d6d116aa4717820f5178a9ab3606b6
Dkyshka <dkyshka25@gmail.com>
2024-10-21 13:32:01 +0500

    added test oferta

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -19,7 +19,7 @@ class SettingSeeder extends Seeder
                 'about' => "Информация о канале",
                 'payment' => "Qulay to'lov turini tanlang:<br><br>*keyingi – siz obunangizni Bot menyusida boshqarishingiz mumkin<br>*Siz obuna tugaguniga qadar bildirishnomalarni olasiz",
                 'public_offer_title' => 'Taklif shartnomasi',
-                'public_offer' => 'https://poddomen.aquabox-client.uz/assets/oferta_tayyor.pdf',
+                'public_offer' => 'https://azbukadoma.kg/files/uploads/opherta.pdf',
                 'tariff_description' => "1 oylik obuna 75 000 so'm<br>3 oylik obuna <s>225 000 so'm</s> 200 000 so'm<br>6 oylik obuna <s>450 000 so'm</s> 400 000 so'm<br>12 oylik obuna <s>900 000 so'm</s> 750 000 so'm<br>",
                 'manager' => 'https://t.me/AndrewManako',
             ]
